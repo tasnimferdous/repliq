b3a25ebf54fe6eee8776b8082575eea2760b0ffb
tasnimferdous <tasnimferdous216@gmail.com>
2023-03-10 19:43:57 +0600

    POST api function to add employee to a company

--- tracker/views.py
+++ tracker/views.py
@@ -11,6 +11,8 @@ def home(request):
     }
     return render(request, 'tracker/home.html', context)
 
+
+# company statistics
 def companyInfo(request,pk):
     company = Company.objects.get(id = pk)
     employees = company.user_set.all()
@@ -21,11 +23,14 @@ def companyInfo(request,pk):
     }
     return render(request, 'tracker/company_info.html', context)
 
+
+# Employee or user registration
 def signUp(request):
     context = {  
     }
     return render(request, 'tracker/registration.html', context)
 
+# Employee or user login
 def signIn(request):
     context = {  
     }
